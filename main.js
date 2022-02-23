4364014363719c2bf2a6bf1d15dc43faef760f1f
Justin Jeffries <justincyjeffries@gmail.com>
2022-02-22 23:53:32 -0700

    commit tree b1175bc34b368c761d03c5041050af85b028e28a

--- main.js
+++ main.js
@@ -3,7 +3,36 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  const like = document.querySelectorAll(".like-glyph")
+ // console.log(like)
+ document.querySelector('#modal').className = "hidden"
 
+
+like.forEach(like => like.addEventListener("click", heartButton))
+
+function heartButton(like){
+ // console.log(like.target)
+ mimicServerCall()
+  .then(() => {
+    if (like.target.innerText === EMPTY_HEART) {
+      like.target.innerText === FULL_HEART
+      like.target.classList.add("activated-heart")
+    }
+    else (
+      like.target.classList.remove("activated-heart"),
+      like.target.innerText = EMPTY_HEART
+      )})
+  .catch(() => {
+    const erMsg = document.getElementById("modal")
+    erMsg.className = "show"
+    setTimeout(() => {
+      const erMsg = document.getElementById("modal")
+    erMsg.className = "hidden"}, 3000);
+    
+    })
+  }
+})
 
 
 
